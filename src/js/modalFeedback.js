6939fc0b2135b069aefee04394978f097abb38dc
tosha <qwerty21042018@mail.ru>
2021-08-16 22:05:27 +0300

    локальная доработка

--- src/js/modalFeedback.js
+++ src/js/modalFeedback.js
@@ -1,7 +1,7 @@
 const feedbackButton = document.querySelectorAll('.modal__feedback--button');
 const modalFeedback = document.querySelector('.modal__feedback');
 const commonContainerBlurFeedback = document.querySelector('.common-container ');
-
+const mainMenu = document.querySelector('.menu-list')
 
 
 for (let i = 0; i < feedbackButton.length; i++){
@@ -12,7 +12,7 @@ for (let i = 0; i < feedbackButton.length; i++){
       e.stopPropagation();
     }
     else {
-
+      mainMenu.style.display='none';
       modalFeedback.classList.add('active__modal');
       commonContainerBlurFeedback.classList.add('blur')
       e.stopPropagation();
